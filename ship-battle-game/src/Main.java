0a6f3679db0b763e83a0aec084f90122247b0e8a
Resul Huseynli <resulhuseynli4@gmail.com>
2025-02-26 23:16:18 +0400

    Checking If Input Number Feature Added!

--- ship-battle-game/src/Main.java
+++ ship-battle-game/src/Main.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
     }
 
@@ -16,6 +21,30 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    private static int isNumber() {
+
+        int number = 0;
+        boolean isNumber = false;
+
+        while (!isNumber) {
+            if (scanner.hasNextInt()) {
+                number = scanner.nextInt();
+                scanner.nextLine();
+
+                if (number > 5 || number < 1) {
+                    System.out.println("You Entered A Number Out of Bound! Please Try Again!");
+                    continue;
+                }
+                isNumber = true;
+
+            } else {
+                System.out.println("You did not enter a number. Try again.");
+                scanner.nextLine();
+            }
+        }
+        return number;
     }
 
 }
