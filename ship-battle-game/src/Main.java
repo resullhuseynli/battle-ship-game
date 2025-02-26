b689657bf88b78674d3ef58b78096e5f60cc61b9
Resul Huseynli <resulhuseynli4@gmail.com>
2025-02-26 23:11:20 +0400

    Ocean Showing Feature Added!

--- ship-battle-game/src/Main.java
+++ ship-battle-game/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
     }
+
+    private static void showTheOcean(char[][] ocean) {
+
+        System.out.println("0 | 1 | 2 | 3 | 4 | 5 |");
+        for (int i = 0; i < ocean.length; i++) {
+            System.out.print(i + 1 + " | ");
+            for (int j = 0; j < ocean[i].length; j++) {
+                if (ocean[i][j] != '!') {
+                    System.out.print(ocean[i][j] + " | ");
+                } else {
+                    System.out.print("- | ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
